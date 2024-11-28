4dac01fd8bc5758d9d662170bbc5eda11357d159
M1NGJU <s2303@e-mirim.hs.kr>
2024-11-28 20:48:29 +0900

    home - home1 말풍선&플러그 위치 조정

--- project/home1.cpp
+++ project/home1.cpp
@@ -8,7 +8,27 @@ int main() {
         return -1;
     }
 
+    sf::Texture RectangleTexture;
+    if (!RectangleTexture.loadFromFile("imgs/start-page1/Rectangle.png")) {
+        return -1; // 이미지 로드 실패 시 종료
+    }
+
+    sf::Texture PolygonTexture;
+    if (!PolygonTexture.loadFromFile("imgs/start-page1/Polygon.png")) {
+        return -1; // 이미지 로드 실패 시 종료
+    }
+
     sf::Sprite backgroundSprite(backgroundTexture);
+    sf::Sprite RectangleSprite(RectangleTexture);
+    sf::Sprite PolygonSprite(PolygonTexture);
+
+    RectangleSprite.setScale(0.9f, 0.7f); // 90% 가로, 70% 세로로 축소
+
+    // 중앙에서 살짝 아래로 이동
+    RectangleSprite.setPosition(310.f, 850.f);
+
+    PolygonSprite.setPosition(1530.f, 927.f);
+
 
     // 메인 루프
     while (window.isOpen()) {
@@ -24,9 +44,12 @@ int main() {
 
         // 배경 이미지 그리기
         window.draw(backgroundSprite);
+        window.draw(RectangleSprite);
+        window.draw(PolygonSprite);
 
         // 화면 출력
         window.display();
+
     }
 
     return 0;
