0ba7ea92cbe5f4e0c5e3ba914c98bed5d620cf01
M1NGJU <s2303@e-mirim.hs.kr>
2024-11-30 10:08:23 +0900

    stage - stage1-3 글자 추가

--- project/stage1-3.h
+++ project/stage1-3.h
@@ -29,6 +29,34 @@ public:
 			return -1; // 이미지 로드 실패 시 종료
 		}
 
+		// 글꼴 로드
+		sf::Font font;
+		if (!font.loadFromFile("font/Jua-Regular.ttf")) { // 사용할 글꼴 경로
+			return -1; // 글꼴 로드 실패 시 종료
+		}
+
+		// 텍스트 생성
+		sf::Text text1;
+		text1.setFont(font);
+		text1.setString(L"미안... 늦었지 빨리 가자");
+		text1.setCharacterSize(40); // 글자 크기
+		text1.setFillColor(sf::Color::Black); // 글자 색상
+		text1.setPosition(1080.f, 400.f); // 텍스트 위치
+
+		sf::Text text2;
+		text2.setFont(font);
+		text2.setString(L"ㅋㅋㅋ새학기 지각 가보자고~");
+		text2.setCharacterSize(40);
+		text2.setFillColor(sf::Color::Black);
+		text2.setPosition(1050.f, 600.f);
+
+		sf::Text text3;
+		text3.setFont(font);
+		text3.setString(L"환웅 호감도 : 50");
+		text3.setCharacterSize(30);
+		text3.setFillColor(sf::Color::Black);
+		text3.setPosition(40.f, 30.f);
+
 		sf::Sprite backgroundSprite(backgroundTexture);
 		sf::Sprite hwanwoongSprite(hwanwoongTexture);
 		sf::Sprite RectangleSprite(RectangleTexture);
@@ -58,6 +86,10 @@ public:
 			window.draw(hwanwoongSprite);
 			window.draw(RectangleSprite);
 			window.draw(RectangleSprite2);
+
+			window.draw(text1);
+			window.draw(text2);
+			window.draw(text3);
 
 			window.display();
 		}
